8544b67ba8ccbce8b114b7e6fdfddbd0b3de919b
Charm Xu <Charm.Xu@nike.com>
2026-04-16 16:12:30 +0800

    Initial Commit

---
+++ SonosWidget/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SonosWidget
+//
+//  Created by Charm Xu on 2026/4/16.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SonosWidget/SonosWidgetApp.swift
@@ -0,0 +1,17 @@
+//
+//  SonosWidgetApp.swift
+//  SonosWidget
+//
+//  Created by Charm Xu on 2026/4/16.
+//
+
+import SwiftUI
+
+@main
+struct SonosWidgetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
